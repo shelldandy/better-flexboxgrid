b61b8df98444e6021d01097216b509c9888b8e1a
mike3run <mpalauzarza@gmail.com>
2015-11-01 01:50:15 -0600

    Added autoprefixer, also removed the dist folder since you can do that with the gulp command anyway

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
 'use strict';
 
 var gulp    = require('gulp'),
-    sass    = require('gulp-sass');
+    sass    = require('gulp-sass'),
+    prefix  = require('gulp-autoprefixer');
 
 
 gulp.task('sass', function () {
   return gulp.src('./src/*.sass')
     .pipe(sass().on('error', sass.logError))
+    .pipe(prefix({
+      browsers : ['last 2 versions']
+    }))
     .pipe(gulp.dest('./dist'));
 });
 
